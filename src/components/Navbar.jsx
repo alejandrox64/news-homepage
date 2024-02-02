30ec66731403537c045a7da7ed4e2234e0946140
Alejandro Rojas <ale402152@gmail.com>
2024-02-02 13:43:23 -0300

    create header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,29 @@ import styles from "../styles/navbar.module.css";
 
 const Navbar = () => {
   return (
-    <div className={styles.nav}>
-      <nav></nav>
-    </div>
+    <nav className={styles.navbar}>
+      <div className={styles.navContainer}>
+        <img src="../../public/images/logo.svg" />
+
+        <ul className={styles.navList}>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">New</a>
+          </li>
+          <li>
+            <a href="#">Popular</a>
+          </li>
+          <li>
+            <a href="#">Trending</a>
+          </li>
+          <li>
+            <a href="#">Categories</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
