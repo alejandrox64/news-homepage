48e3631af343d02ff376374e52931fb6a4121fec
Alejandro Rojas <ale402152@gmail.com>
2024-02-08 22:33:29 -0300

    add other section with more news

---
+++ src/components/MoreNews.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import styles from "../styles/app.module.scss";
+
+const MoreNews = () => {
+  return (
+    <section className={styles.moreNews}>
+      <h1>New</h1>
+      <article>
+        <h2>Hydrogen VS Electric Cars</h2>
+        <p>Will hydrogen-fueled cars ever catch up to EVs?</p>
+      </article>
+      <hr />
+      <article>
+        <h2>The Downsides of AI Artistry</h2>
+        <p>
+          What are the possible adverse effects of on-demand AI image
+          generation?
+        </p>
+      </article>
+      <hr />
+      <article>
+        <h2>Is VC Funding Drying Up?</h2>
+        <p>
+          Private funding by VC firms is down 50% YOY. We take a look at what
+          that means.
+        </p>
+      </article>
+    </section>
+  );
+};
+
+export default MoreNews;
